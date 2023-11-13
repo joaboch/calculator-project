3ae46276fa03406da2859e96967ba2eb30510ae1
joaboch <joakoboch@gmail.com>
2023-11-12 21:01:42 -0300

    Modify code to make it clearer

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const display = document.querySelector('.display');
+
 //Create the calulator basic function operations
 const add = ((num1, num2) => num1 + num2);
 
@@ -18,61 +20,26 @@ const operate = ((num1, num2, operator) => {
 })
 
 
-//Create functions that connect the buttons pressed with the display
-let displayValue = '';
-const buttons = document.querySelector('.buttons');
+//Create an array that holds all the number values 
+const btnNumbers = document.querySelectorAll('[data-numbers]');
 
-function setDisplay(){
-  buttons.addEventListener('click', function (e) {
-    if(e.target.matches('#zero')) {
-      document.querySelector('.display').textContent += '0';
-      displayValue += '0'; 
-    } else if(e.target.matches('#one')) {
-      document.querySelector('.display').textContent += '1';
-      displayValue += '1';
-    } else if(e.target.matches('#two')) {
-      document.querySelector('.display').textContent += '2';
-      displayValue += '2';
-    } else if(e.target.matches('#three')) {
-      document.querySelector('.display').textContent += '3';
-      displayValue += '3';
-    } else if(e.target.matches('#four')) {
-      document.querySelector('.display').textContent += '4';
-      displayValue += '4';
-    } else if(e.target.matches('#five')) {
-      document.querySelector('.display').textContent += '5';
-      displayValue += '5';
-    } else if(e.target.matches('#six')) {
-      document.querySelector('.display').textContent += '6';
-      displayValue += '6';
-    } else if(e.target.matches('#seven')) {
-      document.querySelector('.display').textContent += '7';
-      displayValue += '7';
-    } else if(e.target.matches('#eight')) {
-      document.querySelector('.display').textContent += '8';
-      displayValue += '8';
-    } else if(e.target.matches('#nine')) {
-      document.querySelector('.display').textContent += '9';
-      displayValue += '9';
-    } else if(e.target.matches('#add')) {
-      document.querySelector('.display').textContent += '+';
-      displayValue += '+';
-    } else if(e.target.matches('#subtract')) {
-      document.querySelector('.display').textContent += '-';
-      displayValue += '-';
-    } else if(e.target.matches('#multiply')) {
-      document.querySelector('.display').textContent += '*';
-      displayValue += '*';
-    } else if(e.target.matches('#divide')) {
-      document.querySelector('.display').textContent += '/';
-      displayValue += '/';
-    }
-  }) 
+//Add an event to all the buttons and call a function to add them to the display
+btnNumbers.forEach((button) => {
+  button.addEventListener('click', () => addNumber(button.textContent))
+})
+
+addNumber = (number) => display.textContent += number;
+
+const btnOperators = document.querySelectorAll('[data-operators]')
+
+btnOperators.forEach((button) => {
+  button.addEventListener('click', () => addOperator(button.textContent))
+})
+
+addOperator = (operatorBtns) => {
+  display.textContent += operatorBtns;
 }
 
-function getNumbers() {
-  number1 = displayValue.split('+', 1).join();
-  console.log(typeof(parseFloat(number1)));
-}
 
-setDisplay();
+
+
