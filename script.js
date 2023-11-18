efaa89f833d2ca0485028828f1f8043935c099ad
joaboch <joakoboch@gmail.com>
2023-11-18 18:41:32 -0300

    Fix js bug

--- script.js
+++ script.js
@@ -147,6 +147,7 @@ del.addEventListener('click', () => {
   display.textContent = display.textContent.substring(0, display.textContent.length - 1);
   if(display.textContent == '') {
     display.textContent  = '0';
+    autoReset = true
   }
 })
 
